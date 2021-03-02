22ff65cd63d87b52a2245be55d0234c4e5104617
niuben <1099348621@qq.com>
2021-03-03 03:25:12 +0800

    添加git工程

---
+++ git/src/main/java/com/itheima/pojo/User.java
@@ -0,0 +1,43 @@
+package com.itheima.pojo;
+
+/**
+ * @author ben
+ * @version 1.0.0
+ * @description 说明
+ * @createTime 2021年03月03日 02:50:00
+ */
+public class User {
+
+    private String name;
+
+    private Integer age;
+
+    private String addr;
+
+    public User() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getAddr() {
+        return addr;
+    }
+
+    public void setAddr(String addr) {
+        this.addr = addr;
+    }
+}
